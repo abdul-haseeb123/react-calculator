4de00bfd14ef2cb9d67a8bcf97e8144466e3a2ab
abdul-aziz123 <k224933@nu.edu.pk>
2024-07-30 18:12:54 +0500

    Fixed issues related to input in mobile, refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { cn } from "./lib/utils";
 
 const App = () => {
   const [calculation, setCalculation] = useState("");
-  const [theme, setTheme] = useState<"theme1" | "theme2" | "theme3">("theme3");
+  const [theme, setTheme] = useState<"theme1" | "theme2" | "theme3">("theme1");
 
   const handleThemeChange = () => {
     if (theme == "theme1") {
@@ -16,53 +16,19 @@ const App = () => {
     }
   };
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    let text = "";
-    for (let i = 0; i < e.target.value.length; i++) {
-      const char = e.target.value[i];
-      if (+char) {
-        text += char;
-      } else if (
-        char == "+" ||
-        char == "-" ||
-        char == "*" ||
-        char == "/" ||
-        char == "." ||
-        char == "0"
-      ) {
-        text += char;
-      }
-    }
-    console.log(text);
-    setCalculation(text);
-  }
-
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    try {
-      const result = eval(calculation);
-      if (result == Infinity) {
-        setCalculation("Zero Division Error");
-      } else {
-        setCalculation(result);
-      }
-      console.log(result);
-    } catch (e: unknown) {
-      console.log((e as Error).message);
-    }
-  }
-
   function handleClick(e: React.MouseEvent) {
     const text = e.currentTarget.textContent;
     console.log("calculation", typeof calculation);
     if (text != null) {
       if (text == "DEL") {
-        setCalculation((prevText) => prevText.slice(0, prevText.length - 1));
+        setCalculation(
+          (prevText) => (prevText = prevText.slice(0, prevText.length - 1)),
+        );
       } else if (text == "RESET") {
         setCalculation("");
       } else if (text == "=") {
-        const result = eval(calculation);
-        if (result == Infinity) {
+        const result = String(eval(calculation));
+        if (result == "Infinity") {
           setCalculation("Zero Division Error");
         } else {
           setCalculation(result);
@@ -75,13 +41,16 @@ const App = () => {
 
   return (
     <div
-      className={cn("grid h-screen place-content-center", {
-        "bg-desaturatedblue-700": theme == "theme1",
-        "bg-gray-100": theme == "theme2",
-        "bg-violet-950": theme == "theme3",
-      })}
+      className={cn(
+        "grid h-screen justify-center pt-6 sm:items-center sm:pt-0",
+        {
+          "bg-desaturatedblue-700": theme == "theme1",
+          "bg-gray-100": theme == "theme2",
+          "bg-violet-950": theme == "theme3",
+        },
+      )}
     >
-      <div className="mx-auto flex flex-col gap-4 px-4 sm:w-2/3 sm:px-0">
+      <div className="mx-auto flex w-96 flex-col gap-4 px-4 sm:w-[28rem] sm:px-0">
         <div className="flex w-full justify-between">
           <p
             className={cn("text-2xl font-extrabold", {
@@ -123,7 +92,7 @@ const App = () => {
                     {
                       "left-1 bg-red": theme == "theme1",
                       "left-[22px] bg-orange": theme == "theme2",
-                      "bg-softCyan left-11": theme == "theme3",
+                      "left-11 bg-softCyan": theme == "theme3",
                     },
                   )}
                 ></span>
@@ -131,26 +100,21 @@ const App = () => {
             </div>
           </div>
         </div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            className={cn(
-              "h-24 w-full rounded-lg  px-4 py-2 text-right text-5xl font-bold  caret-transparent shadow-md outline-none",
-              {
-                "bg-desaturatedblue-900 text-white shadow-desaturatedblue-800":
-                  theme == "theme1",
-                "bg-gray-50 text-veryDarkGrayishYellow shadow-gray-150":
-                  theme == "theme2",
-                "bg-violet-900 text-lightYellow shadow-violet-950":
-                  theme == "theme3",
-              },
-            )}
-            value={calculation}
-            onChange={handleChange}
-            autoFocus={true}
-            onBlur={({ target }) => target.focus()}
-          />
-        </form>
+        <div
+          className={cn(
+            "h-24 w-full rounded-lg  px-4 py-2 text-right text-5xl font-bold  caret-transparent shadow-md outline-none",
+            {
+              "bg-desaturatedblue-900 text-white shadow-desaturatedblue-800":
+                theme == "theme1",
+              "bg-gray-50 text-veryDarkGrayishYellow shadow-gray-150":
+                theme == "theme2",
+              "bg-violet-900 text-lightYellow shadow-violet-950":
+                theme == "theme3",
+            },
+          )}
+        >
+          {calculation}
+        </div>
         <div
           className={cn("grid grid-cols-4 gap-5 rounded-lg p-7", {
             "bg-desaturatedblue-800": theme == "theme1",
